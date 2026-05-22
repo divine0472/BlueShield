92acb479844de656fca4088533004e982769764d
divine0472 <140523342+divine0472@users.noreply.github.com>
2026-05-22 14:51:31 +0100

    ee

--- blueshield_scanner.py
+++ blueshield_scanner.py
@@ -442,7 +442,7 @@ BASE_TEMPLATE = """
                 <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"/>
                 <path d="M9 12l2 2 4-4"/>
             </svg>
-            RedShield
+            BlueShield
         </div>
         <nav class="nav-links">
             <a href="/" class="active">Scanner</a>
